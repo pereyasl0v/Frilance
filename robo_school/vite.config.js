342aa2d59595edb557a4cde6d183f5e0ef963406
pereyasl0v <pereyasl0v@ya.ru>
2025-05-10 18:27:50 +0300

    edit path

--- robo_school/vite.config.js
+++ robo_school/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "Frilance/robo_school/",
+	base: "/Frilance/robo_school/",
 	plugins: [react(), tailwindcss()],
 });
