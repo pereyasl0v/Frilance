ca337d98d461557574c9dd63fd7ab2268d103b23
pereyasl0v <pereyasl0v@ya.ru>
2025-05-10 16:44:27 +0300

    mobile change

--- robo_school/src/App.jsx
+++ robo_school/src/App.jsx
@@ -7,36 +7,52 @@ import Trainer from "./trainer";
 function App() {
 	return (
 		<div className='font-jost-regular'>
+			{/* Хэдер */}
 			<div className='sm:w-full sm:min-h-[1080px] sm:max-h-[1080px] bg-[#141024] text-white'>
-				<div className='sm:flex sm:justify-between sm:px-[100px] sm:py-[30px]'>
-					<div className='sm:text-[30px] font-jost-bold'>ROBO.SCHOOL</div>
-					<nav className='sm:flex sm:text-[20px] sm:min-w-[324px] sm:gap-[30px]'>
+				<div className='flex  py-[10px] px-[10px] sm:flex sm:justify-between sm:px-[100px] sm:py-[30px]'>
+					<div className='flex items-center sm:block text-[18px] sm:text-[30px] font-jost-bold'>ROBO.SCHOOL</div>
+					<div className='flex ml-[66px] gap-[10px] sm:hidden'>
+						<div className='bg-[url(/Call.png)]  w-[44px] h-[44px] cursor-pointer'></div>
+						<div className='bg-[url(/Burger.png)] w-[44px] h-[44px] cursor-pointer'></div>
+					</div>
+					<nav className='hidden sm:flex sm:text-[20px] sm:min-w-[324px] sm:gap-[30px]'>
 						<button className='cursor-pointer'>О школе</button>
 						<button className='cursor-pointer'>Тренеры</button>
 						<button className='cursor-pointer'>Стоимость</button>
 					</nav>
-					<div className='sm:text-[30px] font-jost-bold'>+7 800 000 11 22</div>
+					<div className='hidden sm:block sm:text-[30px] font-jost-bold'>+7 800 000 11 22</div>
 				</div>
 
 				<div className='sm:flex sm:pl-[380px]'>
+					{/* Блок с фото */}
 					<div className='sm:flex sm:flex-col sm:gap-[50px] sm:py-[266px]'>
-						<div className='sm:text-[70px] font-jost-bold'>ROBO SCHOOL</div>
-						<div className='sm:text-[20px] sm:w-[478px] sm:h-[64px]'>Курсы повышения квалификации по робототехнике для педагогов начальной школы</div>
-						<button className='sm:w-[260px] sm:h-[79px] bg-[#D52027] rounded-md cursor-pointer'>Записаться на курс</button>
+						<div className='text-[30px] flex mt-[47px] sm:mt-0 justify-center sm:block sm:text-[70px] font-jost-bold'>ROBO SCHOOL</div>
+						<div className='mx-[40px] mt-[10px] sm:m-0 leading-[1.6] text-center sm:text-left  sm:text-[20px] sm:w-[478px] sm:h-[64px]'>
+							Курсы повышения квалификации по робототехнике для педагогов начальной школы
+						</div>
+						<button className=' hidden sm:block sm:w-[260px] sm:h-[79px] bg-[#D52027] rounded-md cursor-pointer'>Записаться на курс</button>
 					</div>
-					<img className='sm:w-[783px] sm:mt-[57px]' src='woman.png' alt='' />
+					<div className='h-[360px]'>
+						<img className='w-[273px] h-[320px] flex m-auto sm:w-[783px] sm:h-min sm:mt-[57px]' src='woman.png' alt='' />
+						<button className='sm:hidden relative top-[-60px] mx-[10px] w-[300px] h-[60px] sm:w-[260px] sm:h-[79px] bg-[#D52027] rounded-md cursor-pointer'>Записаться на курс</button>
+					</div>
 				</div>
 			</div>
 
 			<div className='sm:h-[614px] sm:flex sm:flex-col'>
-				<div className='sm:w-[891px] sm:h-[144px] sm:text-[30px] sm:mt-[120px] sm:ml-[380px]'>
+				<div className='mt-[60px] mx-[10px] text-[20px] sm:w-[891px] sm:h-[144px] sm:text-[30px] sm:mt-[120px] sm:ml-[380px]'>
 					<span className='font-jost-bold'>Robo School </span> – учреждение для формирования кадрового педагогического резерва в сфере робототехники и программирования
 				</div>
-				<div className='sm:flex sm:gap-[40px] sm:ml-[380px] sm:mt-[60px]'>
+				<div className='mt-[40px] mb-[60px] sm:mb-0 sm:flex sm:gap-[40px] sm:ml-[380px] sm:mt-[60px]'>
 					{["10", "20", "100", "10 000"].map((num, index) => (
 						<div key={index}>
-							<div className='sm:text-[70px] font-jost-bold text-[#D52027]'>{num}</div>
-							<div className='sm:text-[18px] sm:w-[260px] sm:h-[58px]'>
+							<div
+								className='text-[60px] mt-[30px] flex justify-center
+							 sm:text-[70px] font-jost-bold text-[#D52027]'
+							>
+								{num}
+							</div>
+							<div className='text-[16px] px-[25px] text-[#141024B2] text-center sm:text-[18px] sm:w-[260px] sm:h-[58px]'>
 								{
 									[
 										"УМК по различным направлениям по робототехнике",
@@ -51,29 +67,49 @@ function App() {
 				</div>
 			</div>
 
-			<div className='sm:h-[485px] bg-[#F9F9F9] sm:flex sm:flex-col'>
-				<div className='sm:mt-[100px] sm:ml-[381px] sm:mb-[60px] sm:text-[50px] font-jost-bold'>Что вы получите после курса</div>
-				<div className='sm:flex sm:ml-[381px] sm:gap-[40px]'>
+			<div className='pt-[40px]   pb-[52px] sm:pb-0 sm:pt-0 sm:h-[485px] bg-[#F9F9F9] sm:flex sm:flex-col'>
+				<div className='text-[30px] text-center sm:text-left sm:mt-[100px] sm:ml-[381px] sm:mb-[60px] sm:text-[50px] font-jost-bold'>Что вы получите после курса</div>
+				<div className='mt-[10px]  sm:mt-0 sm:flex sm:ml-[381px] sm:gap-[40px]'>
 					{[
 						{ title: "Удостоверение", text: "Дающее право преподавать робототехнику для детей 6-12 лет в образовательных учреждениях" },
 						{ title: "Знания", text: "По основам разработки учебно-методических комплексов по робототехнике и программированию" },
 						{ title: "Практику", text: "Возможность пройти практику по преподаванию робототехники на базе R:ED LAB (в оффлайн или онлайн формате)" },
 					].map(({ title, text }, index) => (
 						<div className='sm:flex sm:flex-col' key={index}>
-							<div className='sm:text-[24px] sm:mb-[20px] font-jost-bold flex items-center'>
+							<div className='text-[20px]  mt-[30px] justify-center sm:text-[24px] sm:mb-[20px] font-jost-bold flex items-center'>
 								{title}
-								{title === "Практику" && <div className='bg-[#14102414] sm:text-[11px] sm:min-w-[15px] sm:ml-[10px] rounded-full flex justify-center sm:h-[15px] font-jost-regular'>i</div>}
+								{title === "Практику" && (
+									<div className='bg-[#14102414] text-[16px] relative top-[-5px] right-[-15px] w-[22px] h-[22px] sm:text-[11px] sm:min-w-[15px] sm:ml-[10px] rounded-full flex justify-center items-center sm:h-[15px] font-jost-regular'>
+										i
+									</div>
+								)}
 							</div>
-							<div className='sm:w-[360px] sm:leading-[1.6] sm:text-[18px]'>{text}</div>
+							<div className='text-[#141024B2] text-[16px] text-center px-[10px] mt-[10px] sm:w-[360px] sm:leading-[1.6] sm:text-[18px]'>{text}</div>
 						</div>
 					))}
 				</div>
 			</div>
 
 			<div>
-				<div className='sm:mt-[120px] sm:text-[50px] sm:ml-[380px] font-jost-bold'>Профессиональные тренеры</div>
-				<div className='sm:px-[320px] m-auto'>
+				<div className='text-[30px] text-center mt-[60px] sm:text-left sm:mt-[120px] sm:text-[50px] sm:ml-[380px] font-jost-bold'>Профессиональные тренеры</div>
+				<div className='hidden sm:block sm:px-[320px] m-auto'>
 					<Carousel slidesToShow={3} dotPosition='bottom' dots={{ className: "dots" }} className='sm:py-[60px] sm:px-[60px]' infinite autoplay autoplaySpeed={3600} arrows>
+						{[
+							["1.jpg", "Ирина Лайм", "преподаватель по робототехнике"],
+							["2.jpg", "Марина Орлова", "преподаватель по робототехнике"],
+							["3.jpg", "Максим Петров", "преподаватель по программированию"],
+							["4.jpg", "Константин Назаров", "преподаватель по робототехнике"],
+							["5.jpg", "Лиза Весенняя", "преподаватель по программированию"],
+						].map(([img, name, edu], i) => (
+							<div key={i}>
+								<Trainer imageSrc={img} name={name} education={edu} />
+							</div>
+						))}
+					</Carousel>
+				</div>
+
+				<div className='  py-[20px] sm:hidden  '>
+					<Carousel dotPosition='bottom' dots={{ className: "dots" }} className='sm:py-[60px] sm:px-[60px] pt-[20px] pb-[50px] px-[50px]' infinite autoplay autoplaySpeed={6200}>
 						{[
 							["1.jpg", "Ирина Лайм", "преподаватель по робототехнике"],
 							["2.jpg", "Марина Орлова", "преподаватель по робототехнике"],
--- robo_school/src/trainer.jsx
+++ robo_school/src/trainer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Trainer({ imageSrc, name, education }) {
 	return (
 		<>
-			<img className='h-[500px] w-[360px] object-cover rounded-4xl' src={imageSrc} alt='' />
-			<div className='text-[24px] mt-[20px] font-jost-bold'>{name}</div>
-			<div className='text-[18px] mt-[10px]'>{education}</div>
-			<div className='text-[18px] mt-[20px] cursor-pointer w-max text-[#D52027]'>Подробнее</div>
+			<img className='rounded-2xl h-[306px] w-[220px] w sm:h-[500px] sm:w-[360px] object-cover sm:rounded-4xl justify-center' src={imageSrc} alt='' />
+			<div className='text-[20px] mt-[20px] sm:text-[24px] sm:mt-[20px] font-jost-bold'>{name}</div>
+			<div className='text-[14px] text-[#141024B2] sm:text-[18px] sm:mt-[10px]'>{education}</div>
+			<div className='text-[16px] mt-[20px] sm:text-[18px] sm:mt-[20px] cursor-pointer sm:w-max text-[#D52027]'>Подробнее</div>
 		</>
 	);
 }
